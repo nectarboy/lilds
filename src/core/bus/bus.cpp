cb8985b7d6994db5c385a49f24a9a0603e45c6b0
nectarboy <59830504+nectarboy@users.noreply.github.com>
2025-07-30 01:58:43 -0400

    working on the bus

--- src/core/bus/bus.cpp
+++ src/core/bus/bus.cpp
@@ -1,16 +1,23 @@
 #include "bus.h"
+#include "../arm/memory.h"
 
 namespace Bus {
 
-    u8 State::arm9Read8(Arm::State*, u32 addr, Arm::Access access) {
+    template <Arm::AccessType accessType>
+    u8 State::arm9Read8(Arm::State* arm, u32 addr, Arm::Access access) {
         int region = addr >> 24;
         switch (region) {
             // Main Memory
             case 2: {
+                arm->addMainMemoryWaitstates<accessType, Arm::AccessWidth::Bus16>(access);
                 return mainRam[addr & 0x3fffff];
                 break;
             }
+            default:
+                return 0;
         }
     }
+    template u8 State::arm9Read8<Arm::AccessType::Code>(Arm::State*, u32 addr, Arm::Access access);
+    template u8 State::arm9Read8<Arm::AccessType::Data>(Arm::State*, u32 addr, Arm::Access access);
 
 }
--- src/core/bus/bus.h
+++ src/core/bus/bus.h
@@ -20,10 +20,11 @@ namespace Bus {
         u8 vram[671744];
 
         // memory methods (TODO: 16 and 32 bit reads that dont call 8 reads, is possible probably)
-        u8 arm7Read8(Arm::State*, u32 addr, Arm::Access access);
-        void arm7Write8(Arm::State*, u32 addr, u32 val, Arm::Access access);
-        u8 arm9Read8(Arm::State*, u32 addr, Arm::Access access);
-        void arm9Write8(Arm::State*, u32 addr, u32 val, Arm::Access access);
+        u8 arm7Read8(Arm::State* arm, u32 addr, Arm::Access access);
+        void arm7Write8(Arm::State* arm, u32 addr, u32 val, Arm::Access access);
+        template <Arm::AccessType accessType>
+        u8 arm9Read8(Arm::State* arm, u32 addr, Arm::Access access);
+        void arm9Write8(Arm::State* arm, u32 addr, u32 val, Arm::Access access);
     };
 
 }
